558d08c5c52a639515dab85882ade21ea889e7fe
kelvinjoshua <kelvinbale78@gmail.com>
2021-09-01 19:52:15 +0300

    Bind views to main activity

--- app/src/main/java/com/example/recapped/MainActivity.java
+++ app/src/main/java/com/example/recapped/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.recapped;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button done;
+    public EditText hobbies;
+    public TextView shownHobbies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity2);
+        done = findViewById(R.id.submit);
+        hobbies = findViewById(R.id.hobbies);
+        shownHobbies = findViewById(R.id.shownText);
+
     }
 }
