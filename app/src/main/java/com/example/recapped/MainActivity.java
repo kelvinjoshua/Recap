cb7284d17c57432183af799c79017809b4e2b687
kelvinjoshua <kelvinbale78@gmail.com>
2021-09-02 14:20:41 +0300

    Final refactored code

--- app/src/main/java/com/example/recapped/MainActivity.java
+++ app/src/main/java/com/example/recapped/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.recapped;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -12,38 +13,48 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.recapped.Bio.Data;
+import com.example.recapped.databinding.Activity2Binding;
 import com.google.android.material.snackbar.Snackbar;
 
 import static java.lang.String.format;
 
 public class MainActivity extends AppCompatActivity {
-    public Button done;
-    public EditText hobbies;
-    public TextView shownHobbies;
+    private Activity2Binding binding;
+    public final Data data = new Data();//final to be acceessible in show data method
+    //public Button done;
+    //public EditText hobbies;
+    //public TextView shownHobbies;
     public static  final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity2);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity2);
+        data.setName("Kelvin joshua");//bind the data
+        binding.setData(data); //bind data to the view
+        binding.submit.setOnClickListener(this::showData);
+
+        /*setContentView(R.layout.activity2);
         done = findViewById(R.id.submit);
         hobbies = findViewById(R.id.hobbies);
-        shownHobbies = findViewById(R.id.shownText);
+        shownHobbies = findViewById(R.id.shownText);*/
     }
 
     public void showData(View view) {
+
+        data.setHobbies(format("Your hobbies are: %s", binding.hobbies.getText().toString().trim()));
         //String formatting .format(arguments)
-        shownHobbies.setText(format("Your hobbies are: %s", hobbies.getText().toString().trim()));
-        Snackbar.make(done, R.string.snack,Snackbar.LENGTH_LONG).setAction("MORE", v -> {
-            Log.d(TAG, "Snackbar works effectively");
-        }).show();
+       // shownHobbies.setText(format("Your hobbies are: %s", hobbies.getText().toString().trim()));
+        Snackbar.make(binding.submit, R.string.snack,Snackbar.LENGTH_LONG).setAction("MORE", v -> Log.d(TAG, "Snackbar works effectively")).show();
+        binding.invalidateAll();
         // toggle visibility
-        shownHobbies.setVisibility(View.VISIBLE);
+        binding.hobbies.setVisibility(View.VISIBLE);
         //Hide keyboard after typing - cast to store it in this object
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         //From the view that triggered the keyboard popup signal it to close the keyboard.
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
 
         // Change text color - context compatibility for deprecated getResource()
-        shownHobbies.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.white));
+        binding.hobbies.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.white));
     }
 }
